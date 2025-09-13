20e9f4252ad2a406f3082fafdaad9cb37211cf0b
Vaibhav Rastogi <rastogiv215@gmail.com>
2025-09-13 13:41:27 +0530

    Update frontend.py

--- FastAPI/frontend.py
+++ FastAPI/frontend.py
@@ -11,7 +11,7 @@ age = st.number_input("Age", min_value=1, max_value=119, value=30)
 weight = st.number_input("Weight (kg)", min_value=1.0, value=65.0)
 height = st.number_input("Height (m)", min_value=0.5, max_value=2.5, value=1.7)
 income_lpa = st.number_input("Annual Income (LPA)", min_value=0.1, value=10.0)
-smoker = st.selectbox("Are you Chutia?", options=[True, False])
+smoker = st.selectbox("Are you a Smoker?", options=[True, False])
 city = st.text_input("City", value="Mumbai")
 occupation = st.selectbox(
     "Occupation",
